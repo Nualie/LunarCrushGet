7a40bc0e54ff2723543e0346da9c85f5a0de498d
Nualie <56164296+Nualie@users.noreply.github.com>
2021-10-23 02:22:11 +0200

    attempted to fix connections to API

--- Processor.cs
+++ Processor.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net;
 using System.Net.Http;
 using System.Text;
 using System.Threading.Tasks;
@@ -11,23 +12,54 @@ namespace LunarCrushGet
     {
         public static async Task<Root> LoadInformation()
         {
-            string url = "https://api.lunarcrush.com/v2?data=assets&key=c1s8ov65f4hb4mawe9j1tb&symbol=LTC&data_points=1";
-
-            using (HttpResponseMessage response = await ApiHelper.ApiClient.GetAsync(url))
+            string url = "https://api.lunarcrush.com/v2?data=assets&key=usd1pm5mhrtjoqcn6asxa&symbol=LTC&data_points=1";
+            Console.WriteLine($"General API connection test: {RemoteFileExists("https://api.sunrise-sunset.org/json?lat=41.494804&lng=-75.536852&date=today")} ");
+            try
             {
-                if (response.IsSuccessStatusCode)
+                using (HttpResponseMessage response = await ApiHelper.ApiClient.GetAsync(url))
                 {
-                    Root result = await response.Content.ReadAsAsync<Root>();
+                    if (response.IsSuccessStatusCode)
+                    {
+                        Root result = await response.Content.ReadAsAsync<Root>();
 
-                    return result;
-                }
-                else
-                {
-                    throw new Exception(response.ReasonPhrase);
+                        return result;
+                    }
+                    else
+                    {
+                        throw new Exception(response.ReasonPhrase);
+                    }
                 }
             }
+            catch (Exception e)
+            {
+                Console.WriteLine(e);
+                Console.WriteLine("API failed to connect");
+                return null;
+            }
+
+
         }
+        public static bool RemoteFileExists(string url)
+        {
+            try
+            {
+                //Creating the HttpWebRequest
+                HttpWebRequest request = WebRequest.Create(url) as HttpWebRequest;
+                //Setting the Request method HEAD, you can also use GET too.
+                request.Method = "HEAD";
+                //Getting the Web Response.
+                HttpWebResponse response = request.GetResponse() as HttpWebResponse;
+                //Returns TRUE if the Status code == 200
+                response.Close();
+                return (response.StatusCode == HttpStatusCode.OK);
+            }
+            catch
+            {
+                //Any exception will returns false.
+                return false;
+            }
 
-
-    }
+        }
+    } 
 }
+
--- Program.cs
+++ Program.cs
@@ -8,6 +8,7 @@ using System.Net.Http;
 using System.Net.Http.Headers;
 using System.Data;
 using System.Threading.Tasks;
+using System.Net;
 
 namespace LunarCrushGet
 {
@@ -15,18 +16,49 @@ namespace LunarCrushGet
     {
         static void Main(string[] args)
         {
-            try 
+            string url = "https://api.lunarcrush.com/v2?data=assets&key=usd1pm5mhrtjoqcn6asxa&symbol=LTC";
+            try
             {
+                Root test = new Root();
                 Root.print();
             }
-            catch(Exception e) 
+            catch (Exception e)
             {
                 Console.WriteLine(e);
             }
+            /*
+            if (RemoteFileExists(url))
+            {
+               
+            }
+            else
+            {
+                Console.WriteLine("URL irresponsive");
+            }*/
+            
 
         }
-        
 
+        public static bool RemoteFileExists(string url)
+        {
+            try
+            {
+                //Creating the HttpWebRequest
+                HttpWebRequest request = WebRequest.Create(url) as HttpWebRequest;
+                //Setting the Request method HEAD, you can also use GET too.
+                request.Method = "HEAD";
+                //Getting the Web Response.
+                HttpWebResponse response = request.GetResponse() as HttpWebResponse;
+                //Returns TRUE if the Status code == 200
+                response.Close();
+                return (response.StatusCode == HttpStatusCode.OK);
+            }
+            catch
+            {
+                //Any exception will returns false.
+                return false;
+            }
+        }
 
 
     }
